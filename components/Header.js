2121d7157b0232cfb358e194fbc563ff153820e3
Clément Roig <clm.roig@gmail.com>
2025-03-26 23:02:04 +0100

    fix web component loading

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,18 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <header>
-      <div><img src="/images/osl_logo.png" alt="Onyx Sono Location logo" /></div>
+      <div><img src="images/osl_logo.png" alt="Onyx Sono Location logo" /></div>
       <div>
         <h1>Onyx Sono Location</h1>
         <nav>
           <ul>
-            <li><a href="/">Accueil</a></li>
+            <li><a href="./">Accueil</a></li>
             <li><a href="tarifs.html">Tarifs</a></li>
             <li><a href="contact.html">Contact</a></li>
           </ul>
         </nav>
       </div>
-      <div><img src="/images/osl_logo.png" alt="Onyx Sono Location logo" /></div>
+      <div><img src="images/osl_logo.png" alt="Onyx Sono Location logo" /></div>
     </header>`;
   }
 }
