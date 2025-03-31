696b0be4c5db948cf21f5d03c441fa7ccbc4b720
Clément Roig <clm.roig@gmail.com>
2025-03-31 09:37:36 +0200

    add links in footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,13 @@ class Footer extends HTMLElement {
     const year = new Date().getFullYear();
     this.innerHTML = `
     <footer>
-      <img src="images/logo.png" alt="Onyx Audio logo"/>
+      <div class="links">
+        <a href="mailto:onyx-audio-location@protonmail.com">onyx-audio-location@protonmail.com</a> 
+       <a href="tel:+33643503596">06 43 50 35 96</a> 
+        <a class="social" href="https://www.facebook.com/onyx.audio.location/"><img src="images/facebook_icon.png" alt="Facebook icon"/></a> 
+        <a class="social" href="https://www.instagram.com/onyx.audio.location/"><img src="images/instagram_icon.png" alt="Instagram icon"/></a>
+      </div>
+      <img src="images/logo.png" alt="Onyx Audio logo" class="onyxAudioLogo"/>
 	    <p>&copy;
 		  ${year} Onyx Audio
 	    </p>
