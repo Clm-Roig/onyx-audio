cd4977f5d93f0b0d36d1a29f8b1088a8b8e79118
Clément Roig <clm.roig@gmail.com>
2025-04-06 20:31:43 +0200

    misc responsive stuff

--- components/Footer.js
+++ components/Footer.js
@@ -9,9 +9,11 @@ class Footer extends HTMLElement {
     <footer>
       <div class="links">
         <a href="mailto:onyx-audio-location@protonmail.com">onyx-audio-location@protonmail.com</a> 
-       <a href="tel:+33643503596">06 43 50 35 96</a> 
-        <a class="social" href="https://www.facebook.com/onyx.audio.location/"><img src="images/facebook_icon.png" alt="Facebook icon"/></a> 
-        <a class="social" href="https://www.instagram.com/onyx.audio.location/"><img src="images/instagram_icon.png" alt="Instagram icon"/></a>
+        <a href="tel:+33643503596">06 43 50 35 96</a> 
+        <div class="social-links">
+          <a class="social-link" href="https://www.facebook.com/onyx.audio.location/"><img src="images/facebook_icon.png" alt="Facebook icon"/></a> 
+          <a class="social-link" href="https://www.instagram.com/onyx.audio.location/"><img src="images/instagram_icon.png" alt="Instagram icon"/></a>
+        </div>
       </div>
       <img src="images/logo.png" alt="Onyx Audio logo" class="onyxAudioLogo"/>
 	    <p>&copy;
